d474866f226a43427a3f9dbed77777800ea68eb8
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-05-30 00:42:56 +0600

    searched functionality added

--- Components/Countries.js
+++ Components/Countries.js
@@ -1,24 +1,51 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import React from "react";
 import { useState, useEffect } from "react";
-import { ScrollView } from "react-native-web";
+import { ScrollView } from "react-native";
 import Country from "./Country";
 
 export default function Countries() {
   const [countries, setCountries] = useState([]);
+  const [searched, setSearched] = useState([]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
-      .then((data) => setCountries(data));
+      .then((data) => {
+        setSearched(data);
+        setCountries(data);
+      });
   }, []);
+  const handleChangeText = (text) => {
+    const filtered = countries.filter((country) =>
+      country.name.common.includes(text)
+    );
+    setSearched(filtered);
+  };
   return (
     <View>
-      <Text>Countries: {countries.length}</Text>
+      <Text>Countries: {searched.length}</Text>
+      <Text style={styles.baseText}>Search a Country</Text>
+      <TextInput
+        onChangeText={handleChangeText}
+        style={styles.input}
+      ></TextInput>
       <ScrollView>
-          {
-              countries.map(country => <Country country={country}></Country>)
-          }
+        {searched.map((country) => (
+          <Country country={country}></Country>
+        ))}
       </ScrollView>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  baseText: {
+    margin: 10,
+    fontWeight: "bold",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
--- Components/Country.js
+++ Components/Country.js
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet, TextInput } from "react-native";
+import React from "react";
 
-export default function Country({country}) {
+export default function Country({ country }) {
   return (
     <View>
-      <Text>Country : {country.name.common}</Text>
+
+      <Text style={styles.bigBlue}>Country : {country.name.common}</Text>
+      
+      <Image
+        source={{
+          uri: country.flags.png,
+        }}
+        style={{ width: 350, height: 200 }}
+      />
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+  },
+  bigBlue: {
+    textAlign: "center",
+    color: "blue",
+    fontWeight: "bold",
+    fontSize: 22,
+    backgroundColor: "black",
+    padding: 10,
+  },
+});
